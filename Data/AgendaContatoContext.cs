23a24315a74de03cd23097aa7ae0dcd598701127
Marcos Henrique Mainardi Eckart <marcos.eckart@loglabdigital.com.br>
2021-11-28 16:11:15 -0400

    Implementando .gitignore

--- Data/AgendaContatoContext.cs
+++ Data/AgendaContatoContext.cs
@@ -9,7 +9,7 @@ namespace AgendaContatos.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {   //ALTERAR PARA MÁQUINA DE VOCÊS DATA SOURCE e senha//
-            options.UseSqlServer(connectionString: "Data Source=HELLDER-01;Initial Catalog=AgendaContatos;User ID=sa;Password=123456");
+            options.UseSqlServer(connectionString: "Data Source=127.0.0.1\\ECKART;Initial Catalog=AgendaContatos;User ID=sa;Password=123456");
         }
         public DbSet<AgendaContatoModel> AgendaContato { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
